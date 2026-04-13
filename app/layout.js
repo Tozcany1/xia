55bc09b7329298a2f07150fcdd5a0be4ae6d72bc
Tozcany1 <pipastolucamx@gmail.com>
2026-04-13 14:02:44 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,7 @@
-export const metadata = {
-  title: 'XIA',
-  description: 'No se puede no existe. COSMONTO SEFRIDJE',
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }) {
   return (
-    <html lang="es">
-      <body style={{margin:0, background:'#0a0a0a', color:'#e5e5e5'}}>{children}</body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   )
 }
